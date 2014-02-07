d86ecbc5625266bffe0c885a04fd769ce73ad1f6
David Goodman <goodman.david.h@gmail.com>
2014-02-06 20:45:19 -0800

    Not Simon Says, but expertly completed challange

--- source/simon-says/app/controllers/index.rb
+++ source/simon-says/app/controllers/index.rb
@@ -1,12 +1,16 @@
+require 'json'
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
 
 post '/color' do
+	content_type :json
 
   #Create and return a JSON object with the random cell and color given below.
-
-  cell= rand(1..9)
-  color= "#" + "%06x" % (rand * 0xffffff)
+  {:cell => rand(1..9), :color => "#" + "%06x" % (rand * 0xffffff) }.to_json
 end
+
+
+  # cell= rand(1..9)
+  # color= "#" + "%06x" % (rand * 0xffffff)
--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+	$("#get_color").on("click",function(event) {
+		event.preventDefault;
+		$.ajax ({
+			type: "post",
+			url: "/color"
+		}).done(function(data){
+			console.log(data)
+			$("li").eq(data.cell).css ("background-color", data.color)
 
-});
+
+})})});
