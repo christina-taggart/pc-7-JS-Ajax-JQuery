fc981e529386cb059d4e57e51f83d16beb6f630b
John Olmsted <johnaolmsted@gmail.com>
2014-02-06 16:45:22 -0800

    Adds event handler that initiates AJAX request to change color of cells

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-
+  $('#get_color').on('click', function(event) {
+    event.preventDefault();
+    $.ajax({
+      type: 'post',
+      url: '/color',
+    }).done(function(serverResponse) {
+      changeCellColor(serverResponse.cell, serverResponse.color)
+    }).fail(function() {
+      console.log("Request failed")
+    })
+  })
 });
+
+var changeCellColor = function(cell, color) {
+  $('ul li').eq(cell-1).css('background-color', color)
+}
