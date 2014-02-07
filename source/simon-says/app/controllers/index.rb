39fab7572aa15c4e14c9d2af9ed6e7a9dc84dba7
Patrick <Patrick@Patricks-MacBook-Air.local>
2014-02-06 21:54:21 -0800

    Release 0

--- source/simon-says/app/controllers/index.rb
+++ source/simon-says/app/controllers/index.rb
@@ -1,12 +1,12 @@
+require 'json'
+
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
 post '/color' do
-
-  #Create and return a JSON object with the random cell and color given below.
-
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+  
+  JSON.generate({cell: cell, color: color})
 end
--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-
+  $('#get_color').on("click", function(event){
+    event.preventDefault();
+    getCell();
+    });
 });
+
+function getCell(){
+  $.ajax({
+    type: "POST",
+    url: "/color"
+  }).done(function(response){
+    changeCellColor(response);
+  }).fail(function(response){
+    console.log(response);
+  })
+}
+
+function changeCellColor(response){
+  var data = $.parseJSON(response)
+  var index = data.cell-1
+  $($('li')[index]).css("background-color", data.color);
+}
