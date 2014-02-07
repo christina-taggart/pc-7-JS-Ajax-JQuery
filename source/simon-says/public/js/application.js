953e875dce398a6ad41e00fdecbff5c57d03400f
Spencer Smitherman <spencer.smitherman@gmail.com>
2014-02-06 22:37:48 -0800

    Simon Says works without adjusting DOM

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-
+	$('#get_color').click(function(event){
+		event.preventDefault()
+		myRequest = $.ajax({
+			type: "post",
+			url: '/color',
+			data: $(this).serialize()
+		})
+		myRequest.done(function(serverData){
+			var cell = serverData.cell
+			var color = serverData.color
+			$('li').eq(cell).css('background-color', color)
+		myRequest.fail(function(){
+			console.log('myRequest failed')
+		})
+		})
+	})
 });
