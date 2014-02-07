95161f20fffc504e80aaa31d5a026f7ca36e91b4
Eli Shkurkin <eshkurkin@gmail.com>
2014-02-06 20:10:59 -0800

    add ajax to color cells randomly with random colors

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-
+  $("#get_color").on('click', function(e){
+    e.preventDefault();
+    getBoxAndColor(e);
+  })
 });
+
+function getBoxAndColor(e){
+  $.ajax({
+    type: "POST",
+    url: "/color"
+  }).done(function(response){
+    colorBox(response);
+  }).fail(function(){
+    console.log("Request failed");
+  })
+}
+
+function colorBox(response){
+  var parsed = $.parseJSON(response)
+  $($('li')[parsed.cell - 1]).css({"background-color":parsed.color});
+}
+
+
