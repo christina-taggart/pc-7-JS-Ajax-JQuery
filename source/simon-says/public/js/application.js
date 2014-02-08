79c86479add41716ca0e556568c46d333f540282
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-08 10:03:01 -0800

    Click button now changes a random square into a random color, no HTML modification (this is release 0)

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
 $('#get_color').on("click", function(event) {
   event.preventDefault();
   $.post("/color", function(result_data){
-    debugger
-    $('ul li:eq(1)').css('background-color', result_data.random_color);
+    $('ul li:eq('+result_data.random_cell+')').css('background-color', result_data.random_color);
   });
 })
 
