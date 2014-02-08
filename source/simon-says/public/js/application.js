ecfc7652557c6e8d2d1515649d3502b89d4b1388
Darcey Lachtman <dslachtman@gmail.com>
2014-02-08 15:38:58 -0800

    add jquery and ajax

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+	$('#get_color').on('click', function(e){
+		e.preventDefault();
+		$.ajax({
+			type: 'POST',
+			url:'/color',
+
+		}).done(function(colors){
+			$('li').eq(colors.cell).css ("background-color", colors.color)
+		})
+
+	})
 
 });
