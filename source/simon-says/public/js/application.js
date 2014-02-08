291a7f3c2777cc1e669e7c4422e09be311f3c286
Ricardo Rubio <rickarubio@gmail.com>
2014-02-08 15:00:55 -0800

    Add event handler to link element and fire off an ajax post to the controller.

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+  var boxes = $('#color_me li');
+
+  $('#get_color').on('click', function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: '/color'
+    }).done(function(server_response) {
+      alert(server_response);
+    }) 
+  });
 
 });
