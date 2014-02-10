149f52352f387db09a96880741b7652412368283
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-09 17:52:16 -0800

    Refactor of DOM manipulation for readability

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 $('#get_color').on("click", function(event) {
   event.preventDefault();
   $.post("/color", function(result_data){
-    $('ul li:eq('+result_data.random_cell+')').css('background-color', result_data.random_color);
+    $($('ul li')[result_data.random_cell]).css('background-color', result_data.random_color);
   });
 })
 
