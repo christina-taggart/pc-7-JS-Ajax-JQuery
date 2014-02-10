c339954836ad2606cba6a745fdd19bc158f45f44
hzla00 <neohzla@gmail.com>
2014-02-09 18:34:43 -0800

    added ajax to make the color button thing work

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+	$('#get_color').on("click", function(e){
+		e.preventDefault();
+		$.ajax({
+			type: 'post',
+			url: '/color'
+		}).done(function(json){
+			var object = $.parseJSON(json)
+			$('li:nth-child(' + object.cell + ')').css('background-color', object.color)
+		})
+	})
 });
