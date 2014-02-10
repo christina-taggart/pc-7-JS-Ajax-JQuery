0bf8df4ee66d9f9993604bf1c4d28ded6e611ded
Emmanuel Kaunitz <ekaunitz@gmail.com>
2014-02-09 20:50:16 -0800

    created json object in post route to instruct javascript which LI to change to which color. Uses asynchronous  requests using jquery ajax method.

--- source/simon-says/app/controllers/index.rb
+++ source/simon-says/app/controllers/index.rb
@@ -1,12 +1,15 @@
+require 'json'
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
 
 post '/color' do
+  content_type :json
 
   #Create and return a JSON object with the random cell and color given below.
 
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+  json_hash = {color: color, cell: cell}.to_json
 end
--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 
+	//shit goes in here to color shit
+
+	$("#get_color").on("click", function(event) {
+		$.ajax({
+			type: "POST",
+			url: '/color'
+		})
+		.done(function(json_hash){
+			$( "#color_me li" ).eq(json_hash.cell -1).css("background-color", json_hash.color);
+		})
+	})
 });
