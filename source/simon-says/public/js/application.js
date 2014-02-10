cc4cf05cc650f89d3f34d61f98aab88687ad830d
Emily Foley <foleyemilym@gmail.com>
2014-02-09 20:42:12 -0800

    Ajax implemented, JSON should be working.

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-
+  $("#get_color").on("click", function(){
+    $.ajax({
+      type:"POST"
+      url: '/color'
+    })
+    .done(function(){
+      $('#color_me li:nth-child('cell')').css('background-color', color)
+    })
+  })
 });
