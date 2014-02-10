1876c3d1c83d10213e3a6b832ba710d0495fd40a
Christina Taggart <ctaggart@Christinas-MacBook-Pro-2.local>
2014-02-09 16:44:05 -0800

    Release 0: JS Ajax and JQuery to simulate ongoing SimonSez game

--- source/simon-says/app/controllers/index.rb
+++ source/simon-says/app/controllers/index.rb
@@ -3,10 +3,10 @@ get '/' do
   erb :index
 end
 
-post '/color' do
-
+get '/color' do
   #Create and return a JSON object with the random cell and color given below.
 
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+  { cell: cell, color: color }.to_json
 end
--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
 
+  $('#get_color').on('click', getColor );
+
+  function getColor(){
+    console.log(this)
+    $.get("/color").done( function(response) {
+      response = JSON.parse(response)
+      var square = $('#color_me li')[response.cell];
+      $(square).css('background-color', response.color);
+      setTimeout(getColor, 1000);
+    })
+  }
+
 });
