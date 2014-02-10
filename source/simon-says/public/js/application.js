659043f0364bb8465405e00b90d5fde4757a6dad
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-09 19:18:07 -0800

    Renamed variables for clarification

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 $('#get_color').on("click", function(event) {
   event.preventDefault();
-  $.post("/color", function(result_data){
-    $('li')[result_data.random_cell].style.background=result_data.random_color
+  $.post("/color", function(response){
+    $('li')[response.random_cell].style.background=response.random_color
   });
 })
 
