75db411d9b5de548fe5f54bbef45f02d3d08225b
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-09 19:13:38 -0800

    Refactor DOM modification code

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 $('#get_color').on("click", function(event) {
   event.preventDefault();
   $.post("/color", function(result_data){
-    $($('ul li')[result_data.random_cell]).css('background-color', result_data.random_color);
+    $('li')[result_data.random_cell].style.background=result_data.random_color
   });
 })
 
