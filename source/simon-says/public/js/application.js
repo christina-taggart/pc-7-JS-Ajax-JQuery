b6c416e0da3fb2990a220a69cb0a8b029cdade94
Elliot Chalfant <null>
2014-02-11 15:52:11 -0800

    changes colors of li_s with ajax requests

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,12 +1,13 @@
 
 $(document).ready(function(){
-  $('a').on('click', function(event) {
+  $('#get_color').on('click', function(event) {
     event.preventDefault();
     $.ajax({
       type: 'POST',
       url: '/color'
     }).done(function(serverResponse){
-      console.log(serverResponse)
+      parsed = JSON.parse(serverResponse);
+      $('ul li').eq(parsed.cell-1).css('background-color', parsed.color)
     })
   })
 });
