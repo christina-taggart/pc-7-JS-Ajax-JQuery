9a2b807ea4ddf37d65a79c04f55bf62af14e58eb
Eli Shkurkin <eshkurkin@gmail.com>
2014-02-10 21:26:15 -0800

    Changes random cell to a random color. No OO design yet

--- source/simon-says/app/controllers/index.rb
+++ source/simon-says/app/controllers/index.rb
@@ -1,12 +1,12 @@
+require 'json'
+
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
 post '/color' do
-
-  #Create and return a JSON object with the random cell and color given below.
-
-  cell= rand(1..9)
+  content_type :json
+  cell= rand(0..8)
   color= "#" + "%06x" % (rand * 0xffffff)
+  {cell: cell, color: color}.to_json
 end
--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+  $('#get_color').on('click', function(e) {
+    e.preventDefault();
+    colorCell();
+  });
 
+  function colorCell(){
+    $.ajax({
+      type: "POST",
+      url: "/color"
+    }).done(function(data){
+      updateCellColor(data);
+    });
+
+    function updateCellColor(data){
+      $('li')[data.cell].style.background=data.color;
+    }
+  }
 });
