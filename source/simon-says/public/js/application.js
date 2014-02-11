11cddf834a749602dba1a7657aeaef90d7600283
Emily Foley <foleyemilym@gmail.com>
2014-02-10 22:45:03 -0800

    Ajax working, colors changing appropriately. Good to go.

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-  $("#get_color").on("click", function(){
+  $("#get_color").on("click", function(e){
+    e.preventDefault;
     $.ajax({
-      type:"POST"
-      url: '/color'
+      type:'POST',
+      url:'/color'
     })
-    .done(function(){
-      $('#color_me li:nth-child('cell')').css('background-color', color)
+    .done(function(response){
+      $('#color_me li').eq(response.cell).css('background-color', response.color)
     })
   })
 });
