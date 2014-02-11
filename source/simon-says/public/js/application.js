e36970e4d28271f11f93896e904a62a13f65e6fc
unknown <h_hsu817@yahoo.com>
2014-02-11 13:00:21 -0800

    Release 0: Squares change color on button click

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
 
-});
+  $('#get_color').on("click",function(event){
+    event.preventDefault()
+    colorChange()
+  })
+
+    function colorChange(){
+    $.ajax({
+      url: '/color',
+      type: 'post'
+    }).done(function(square){
+    	// console.log(square)
+      $("#color_me li").eq(square.cell-1).css("background", square.color)
+    })
+	}
+})
