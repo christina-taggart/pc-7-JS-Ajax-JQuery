db88dcb001f107ce357ea8f6962c48e9e6dded69
Elliot Chalfant <null>
2014-02-11 13:46:08 -0800

    sends ajax request from link click and logs the respsonse from the server

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-
+  $('a').on('click', function(serverResponse) {
+    $.ajax({
+      type: 'POST',
+      url: '/color'
+    }).done(
+      console.log(serverResponse)
+    )
+  })
 });
