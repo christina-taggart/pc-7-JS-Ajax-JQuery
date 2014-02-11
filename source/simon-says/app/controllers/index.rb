ad44c2537dafd94efff973ee3a62034271058234
Aditya Mahesh & Emmanuel Kaunitz <pair+amahesh0501.mechamanny@devbootcamp.com>
2014-02-10 22:25:32 -0800

    Updated JS so cells update colors using JSON

--- source/simon-says/app/controllers/index.rb
+++ source/simon-says/app/controllers/index.rb
@@ -6,7 +6,7 @@ end
 post '/color' do
 
   #Create and return a JSON object with the random cell and color given below.
-
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+  {:cell => cell, :color => color}.to_json
 end
--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+  $('#get_color').on('click', function(e){
+    e.preventDefault();
+    var request = $.ajax({
+      type: "POST",
+      url: "/color",
+      dataType: "json"
+    });
 
+    request.done(function(response){
+      $('#color_me li').eq(response["cell"]).css('background-color', response["color"])
+    })
+  })
 });
