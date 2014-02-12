df3956aba45ac0cc601df402dd0d62f8126ec42e
Ricardo Rubio <rickarubio@gmail.com>
2014-02-11 17:37:08 -0800

    Refactored to keep variable declaration closer to its use as per @sgharms suggestion for refactor.

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-  var boxes = $('#color_me li');
-
+  
   $('#get_color').on('click', function(e) {
     e.preventDefault();
     $.ajax({
       type: "POST",
       url: '/color'
     }).done(function(data) {
+      var boxes = $('#color_me li');
       $(boxes[data.cell]).css('background-color', data.color);
     }) 
   });
